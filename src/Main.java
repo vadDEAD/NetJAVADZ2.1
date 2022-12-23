a71ed9449dc2d00787eb4a3e8032f7437f4daa54
vadDEAD <118129616+vadDEAD@users.noreply.github.com>
2022-12-23 23:01:25 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,15 @@
 public class Main {
     public static void main(String[] args) {
 
-        int Ticket_cost = 1200; // цена билета
-        int Mile_per_Ticket = 20; // миль за билет//
-        int Bonus_miles; // бесплатные мили
-        if (Ticket_cost <= 0) // ошибка 0
+        int ticketCost = 1200; // цена билета
+        int milePerTicket = 20; // миль за билет//
+        int bonusMiles; // бесплатные мили
+        if (ticketCost <= 0) // ошибка 0
         {
             System.out.println("Ошибка. Стоимость билета не может равняться или быть меньше нуля");
         } else {
-            Bonus_miles = Ticket_cost / Mile_per_Ticket; // формула расчета
-            System.out.println("Стоимость вашего билета:" + Ticket_cost + "р. Ваши бонусные милии состовляют " + Bonus_miles);
+            bonusMiles = ticketCost / milePerTicket; // формула расчета
+            System.out.println("Стоимость вашего билета:" + ticketCost + "р. Ваши бонусные милии состовляют " + bonusMiles);
         }
 
 
